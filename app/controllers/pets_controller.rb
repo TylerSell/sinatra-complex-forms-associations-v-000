bef684db065580c8ad1bf64e97ac045f026ba8c4
Tyler Sell <tylerselldesign@gmail.com>
2019-02-28 05:33:46 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,6 +25,11 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["pet"]["name"].empty?
+      @pet.owner << Owner.create(name: params["owner"]["name"])
+    end
 
     redirect to "pets/#{@pet.id}"
   end
